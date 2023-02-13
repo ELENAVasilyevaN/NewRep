bb5a52e1b5067ec21956354f7c3b6a73afb74c0d
«Elena <tetlada@mail.ru>
2023-02-13 23:20:54 +0300

    Zadacha27

---
+++ Example27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+int SumNum(int Num)
+{ 
+    int res = 0; 
+    while (Num > 0) 
+    { 
+                res = res + Num % 10;
+        Num = Num / 10;
+    }
+return res;
+}
+Console.Clear();
+Console.Write("Введите число N: ");
+int Num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр в числе {Num} равна {SumNum(Num)}");
+
