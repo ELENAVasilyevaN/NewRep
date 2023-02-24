82401c822f6766e6a5f20dc65247c90da12731cb
«Elena <tetlada@mail.ru>
2023-02-24 22:36:56 +0300

    Zadacha50

---
+++ Example50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");// табуляция
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("row = ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("col = ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+
+Console.WriteLine("Введите позицию элемента");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if (a > (row) && b > (col))
+    Console.WriteLine("Такая позиция отсутствует");
+else
+{
+    object c = array2D[(a-1), (b-1)];
+    Console.WriteLine(c);
+}
